a0ec44fac68b6ccc215a943c664940beadecd5b9
eagle1111 <wang499817054@gmail.com>
2017-07-07 17:56:47 +0800

    implement sendconversation

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :friendships
   resources :users, only: [:index]
 
-  resources :conversations do
+  resources :conversations, only: [:create] do
     resources :messages
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
