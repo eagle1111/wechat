12ec6034124ef6f2cefb6d90deefff0a09ae43c4
eagle1111 <wang499817054@gmail.com>
2017-07-07 15:28:02 +0800

    implement friendship and conversation and message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'friendships#index'
+
+  resources :friendships
+  resources :users, only: [:index]
+
+  resources :conversations do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
